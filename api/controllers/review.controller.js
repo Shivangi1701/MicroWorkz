2bf22177c102734158752cd1c29e3966e317de19
Shivangi1701 <shivangi170102@gmail.com>
2023-06-22 20:13:39 +0530

    api endpoint of creating review working

--- api/controllers/review.controller.js
+++ api/controllers/review.controller.js
@@ -1,1 +1,50 @@
-export const fn = (req, res) => {};
+import createError from "../utils/createError.js";
+import Review from "../models/review.model.js";
+import Gig from "../models/gig.model.js";
+
+export const createReview = async (req, res, next) => {
+  if (req.isSeller)
+    return next(createError(403, "Sellers cannot create a review"));
+  const newReview = new Review({
+    userId: req.userId, // userId of the who created request of creating a review
+    gigId: req.body.gigId,
+    desc: req.body.desc,
+    star: req.body.star,
+  });
+  try {
+    const review = await Review.findOne({
+      gigId: req.body.gigId,
+      userId: req.body.userId,
+    });
+    // if already made a review
+    if (review)
+      return next(
+        createError(403, "You have already created a review on this gig")
+      );
+
+    const savedReview = await newReview.save();
+    // stars are given to gig hence needs an update
+    await Gig.findByIdAndUpdate(req.body.gigId, {
+      $inc: { totalStars: req.body.star, starNumber: 1 },
+    });
+    res.status(201).send(savedReview);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getReviews = async (req, res) => {
+  try {
+    const reviews = await Review.find({ gigId: req.body.gigId }); // all reviews on gig with gigId given
+    res.status(200).send(reviews);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteReview = async (req, res) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/review.route.js
+++ api/routes/review.route.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { verifyToken } from "../middleware/jwt_middleware.js";
+import {
+  createReview,
+  getReviews,
+  deleteReview,
+} from "../controllers/review.controller.js";
 
 const router = express.Router();
 
-router.get("/test");
+router.post("/", verifyToken, createReview);
+router.get("/:id", getReviews);
+router.delete("/:id", verifyToken, deleteReview);
 
 export default router;
