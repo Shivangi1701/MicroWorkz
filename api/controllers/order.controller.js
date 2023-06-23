e5b9cf9ce008fb8d4e7b8bc782c3602b14d22f84
Shivangi1701 <shivangi170102@gmail.com>
2023-06-23 12:35:48 +0530

    create order function

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,7 +1,23 @@
-import createError from "../utils/createError";
+import Order from "../models/order.model.js";
+import Gig from "../models/gig.model.js";
+import createError from "../utils/createError.js";
 
 export const createOrder = async (req, res, next) => {
   try {
+    const gig = await Gig.findById(req.params.gigId); // find gig in db by using id that user has requested
+
+    const newOrder = new Order({
+      gigId: gig._id, // object id of gig is stored as gigId in new order object
+      img: gig.cover,
+      title: gig.title,
+      buyerId: req.userId, // user is buyer who made an order request
+      sellerId: gig.userId, // gig has userId of seller the one who created it
+      price: gig.price,
+      payment_intent: "temporary",
+    });
+
+    await newOrder.save();
+    res.status(201).send("order successful");
   } catch (err) {
     next(err);
   }
