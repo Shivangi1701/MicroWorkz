2eeac2bcf731ee55370b05066de59fe6d88a8867
Shivangi1701 <shivangi170102@gmail.com>
2023-06-23 18:07:00 +0530

    update conv. function done

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -1,6 +1,54 @@
 import createError from "../utils/createError.js";
+import Conversation from "../models/conversation.model.js";
 
-export const fn = async (req, res, next) => {
+export const createConversation = async (req, res, next) => {
+  try {
+    const newConversation = new Conversation({
+      // first one is seller Id always
+      // the person initiating conv is seller then req.userId first
+      // if person initiating conv is user then (req.body.to) first i.e., seller first
+      id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
+      sellerId: req.isSeller ? req.userId : req.body.to,
+      buyerId: req.isSeller ? req.body.to : req.userId,
+      readBySeller: req.isSeller, // if seller has send it ..it has read it hence true else false
+      readByBuyer: !req.isSeller,
+    });
+
+    const savedConversation = await newConversation.save();
+    res.status(201).send(savedConversation);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateConversation = async (req, res, next) => {
+  try {
+    const updatedConversation = await Conversation.findOneAndUpdate(
+      { id: req.params.id },
+      {
+        $set: {
+          // after finding the conv. now we can update using set method
+          readBySeller: req.isSeller,
+          readByBuyer: !req.isSeller,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).send(updatedConversation);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getSingleConversation = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getConversations = async (req, res, next) => {
   try {
   } catch (err) {
     next(err);
--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,7 +1,17 @@
 import express from "express";
+import { verifyToken } from "../middleware/jwt_middleware.js";
+import {
+  getConversations,
+  createConversation,
+  getSingleConversation,
+  updateConversation,
+} from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
-router.get("/test");
+router.get("/", verifyToken, getConversations);
+router.post("/", verifyToken, createConversation);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.patch("/:id", verifyToken, updateConversation);
 
 export default router;
