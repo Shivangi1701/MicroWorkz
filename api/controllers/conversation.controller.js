a116ecb4678c0ab3d79da338498fa976ee721c70
Shivangi1701 <shivangi170102@gmail.com>
2023-06-23 18:10:52 +0530

    get single conv. function

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -43,6 +43,8 @@ export const updateConversation = async (req, res, next) => {
 
 export const getSingleConversation = async (req, res, next) => {
   try {
+    const conversation = await Conversation.findOne({ id: req.params.id });
+    res.status(200).send(conversation);
   } catch (err) {
     next(err);
   }
