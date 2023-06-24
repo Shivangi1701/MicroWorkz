a193cd3bd9ec213aa9976bdede5b8d791c986ddc
Shivangi1701 <shivangi170102@gmail.com>
2023-06-24 16:33:53 +0530

    redirecting to orders page & refreshing the page function

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -4,12 +4,12 @@ import createError from "../utils/createError.js";
 import Stripe from "stripe";
 
 export const intent = async (req, res, next) => {
-  const stripe = new Stripe(process.env.STRIPE); // secret key in .env file
+  const stripe = new Stripe(process.env.STRIPE);
 
   const gig = await Gig.findById(req.params.id);
-  // Create a PaymentIntent with amount and currency
+
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: gig.price,
+    amount: gig.price * 100,
     currency: "inr",
     automatic_payment_methods: {
       enabled: true,
@@ -17,11 +17,11 @@ export const intent = async (req, res, next) => {
   });
 
   const newOrder = new Order({
-    gigId: gig._id, // object id of gig is stored as gigId in new order object
+    gigId: gig._id,
     image: gig.cover,
     title: gig.title,
-    buyerId: req.userId, // user is buyer who made an order request
-    sellerId: gig.userId, // gig has userId of seller the one who created it
+    buyerId: req.userId,
+    sellerId: gig.userId,
     price: gig.price,
     payment_intent: paymentIntent.id,
   });
@@ -42,6 +42,24 @@ export const getOrders = async (req, res, next) => {
     });
 
     res.status(200).send(orders);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const confirm = async (req, res, next) => {
+  try {
+    const orders = await Order.findOneAndUpdate(
+      {
+        payment_intent: req.body.payment_intent,
+      },
+      {
+        $set: {
+          isCompleted: true,
+        },
+      }
+    );
+    res.status(200).send("Order has been confirmed!");
   } catch (err) {
     next(err);
   }
--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleware/jwt_middleware.js";
-import { getOrders, intent } from "../controllers/order.controller.js";
+import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 //router.post("/:gigId", verifyToken, createOrder); // no use after payment-intent function
 router.get("/", verifyToken, getOrders);
 router.post("/create-payment-intent/:id", verifyToken, intent);
+router.put("/", verifyToken, confirm);
 
 export default router;
