b80ee369b9cdd05e334cea53c3a1b14c308e9c90
Shivangi1701 <shivangi170102@gmail.com>
2023-06-24 11:08:42 +0530

    payment intent function

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -6,31 +6,30 @@ import Stripe from "stripe";
 export const intent = async (req, res, next) => {
   const stripe = new Stripe(process.env.STRIPE); // secret key in .env file
 
+  const gig = await Gig.findById(req.params.id);
   // Create a PaymentIntent with amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 
-  })
-};
+    amount: gig.price,
+    currency: "inr",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
 
-export const createOrder = async (req, res, next) => {
-  try {
-    const gig = await Gig.findById(req.params.gigId); // find gig in db by using id that user has requested
+  const newOrder = new Order({
+    gigId: gig._id, // object id of gig is stored as gigId in new order object
+    image: gig.cover,
+    title: gig.title,
+    buyerId: req.userId, // user is buyer who made an order request
+    sellerId: gig.userId, // gig has userId of seller the one who created it
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
 
-    const newOrder = new Order({
-      gigId: gig._id, // object id of gig is stored as gigId in new order object
-      image: gig.cover,
-      title: gig.title,
-      buyerId: req.userId, // user is buyer who made an order request
-      sellerId: gig.userId, // gig has userId of seller the one who created it
-      price: gig.price,
-      payment_intent: "temporary",
-    });
-
-    await newOrder.save();
-    res.status(201).send("order successful");
-  } catch (err) {
-    next(err);
-  }
+  await newOrder.save();
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
 };
 
 export const getOrders = async (req, res, next) => {
